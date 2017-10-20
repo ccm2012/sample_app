f32e4cb541d677e4d03cd8ace3e0aec927d601ef
ccm2012 <ccm.hsss.2012@gmail.com>
2017-10-20 14:50:20 +0700

    Chap 9

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,8 +3,26 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember user
+    cookies.permanent.signed[:user_id] = user.id
+    remember_next user.remember_token
+  end
+
+  def remember_next token
+    cookies.permanent[:remember_token] = token
+  end
+
   def current_user
-    @current_user ||= User.find_by id: session[:user_id]
+    if (user_id_session = session[:user_id])
+      @current_user ||= User.find_by id: user_id_session
+    elsif (user_id_cookie = cookies.signed[:user_id])
+      user = User.find_by id: user_id_cookie
+
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   def logged_in?
@@ -12,7 +30,14 @@ module SessionsHelper
   end
 
   def log_out
+    forget current_user
     session.delete :user_id
     @current_user = nil
+  end
+
+  def forget user
+    user.forget
+    cookies.delete :user_id
+    cookies.delete :remember_token
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_reader :remember_token
+
   before_save :downcase_email
 
   validates :name, presence: true, length: {maximum: 50}
@@ -15,6 +17,20 @@ class User < ApplicationRecord
     self.email = email.downcase
   end
 
+  def remember
+    @remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest.present?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
+
   class << self
     def digest string
       cost =
@@ -24,6 +40,10 @@ class User < ApplicationRecord
           BCrypt::Engine.cost
         end
       BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
     end
   end
 end
