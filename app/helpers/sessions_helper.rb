273731f395595b223fdc504e52ac6b385bec46ed
ccm2012 <ccm.hsss.2012@gmail.com>
2017-10-24 13:20:39 +0700

    Chap 10

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,15 +29,24 @@ module SessionsHelper
     current_user.present?
   end
 
+  def forget user
+    user.forget
+    cookies.delete :user_id
+    cookies.delete :remember_token
+  end
+
   def log_out
     forget current_user
     session.delete :user_id
     @current_user = nil
   end
 
-  def forget user
-    user.forget
-    cookies.delete :user_id
-    cookies.delete :remember_token
+  def redirect_back_or default
+    redirect_to session[:forwarding_url] || default
+    session.delete :forwarding_url
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
